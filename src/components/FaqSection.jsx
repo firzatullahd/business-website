80bd3c8e4bc4f333b2840833b1dbef550635dc68
firzatullahd <firzatullahdwikoramadhan@gmail.com>
2021-01-15 15:19:12 +0700

    first commit

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+            </div>
+        </div>
+    );
+}
+
+export default FaqSection;
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+import react, {Fragment} from 'react';
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+
+const AboutUs =() => {
+    return(
+        <Fragment>
+            <AboutSection />
+            <ServicesSection />
+        </Fragment>
+    );
+}
+
+export default AboutUs;
